e2f00168316007bf089a516764514b866e01a321
daniikhan23 <daniyal.ashraf.2021@gmail.com>
2024-09-30 14:17:44 +0500

    Initialised Tailwind CSS

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,14 @@
 import "./styles/App.css";
 
 function App() {
-  return <h1>Welcome to Thief's Escape!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Welcome to Thief's Escape!
+      </h1>
+      <h2>This is a second title here!</h2>
+    </>
+  );
 }
 
 export default App;
